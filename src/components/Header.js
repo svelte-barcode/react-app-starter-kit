9be45c029d8b60a7c2d476e40f9cfcf9b2bce503
Bunlong <bunlong.van@gmail.com>
2018-02-24 18:28:33 +0700

    add responsive header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,7 @@
 import React from 'react';
 
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
-
 const Header = ({  classes }) => {
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="title" color="inherit">
-          Project Name
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  );
+  return "Header";
 };
 
 export default Header
--- src/views/layouts/App.js
+++ src/views/layouts/App.js
@@ -1,20 +1,158 @@
 import React from 'react';
 import Grid from 'material-ui/Grid';
 import Header from '../../components/Header';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
 
-const App = props => ({
-  render() {
-    return (
-      <div>
-        <Grid container>
-          <Header />
-          <main>
-            {props.children}
-          </main>
-        </Grid>
-      </div>
-    );
-  }
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
+import Typography from 'material-ui/Typography';
+import Hidden from 'material-ui/Hidden';
+import Drawer from 'material-ui/Drawer';
+import Divider from 'material-ui/Divider';
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    height: '100%',
+    zIndex: 1,
+    overflow: 'hidden'
+  },
+  appFrame: {
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+  },
+  appBar: {
+    position: 'absolute',
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+  },
+  navIconHide: {
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+  drawerHeader: theme.mixins.toolbar,
+  drawerPaper: {
+    width: 250,
+    [theme.breakpoints.up('md')]: {
+      width: drawerWidth,
+      position: 'relative',
+      height: '100%',
+    },
+  },
+  content: {
+    backgroundColor: theme.palette.background.default,
+    width: '100%',
+    padding: theme.spacing.unit * 3,
+    height: 'calc(100% - 56px)',
+    marginTop: 56,
+    [theme.breakpoints.up('sm')]: {
+      height: 'calc(100% - 64px)',
+      marginTop: 64,
+    },
+  },
 });
 
-export default App;
+class App extends React.Component {
+  state = {
+    mobileOpen: false,
+  };
+
+  handleDrawerToggle = () => {
+    this.setState({ mobileOpen: !this.state.mobileOpen });
+  };
+
+  render() {
+    const { classes, theme } = this.props;
+
+    const drawer = (
+      <div>
+        <div className={classes.drawerHeader}>
+        </div>
+        <Divider />
+        <List>
+          <ListItem button>
+            <ListItemText primary="Dashboard" />
+          </ListItem>
+        </List>
+        <Divider />
+        <List>
+          <ListItem button>
+            <ListItemText primary="Trash" />
+          </ListItem>
+        </List>
+      </div>
+    );
+
+    return (
+      <Grid container>
+        <div className={classes.root}>
+          <div className={classes.appFrame}>
+            <AppBar className={classes.appBar}>
+              <Toolbar>
+                <IconButton
+                  color="inherit"
+                  aria-label="open drawer"
+                  onClick={this.handleDrawerToggle}
+                  className={classes.navIconHide}
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="title" color="inherit" noWrap>
+                  Dashboard
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <Hidden mdUp>
+              <Drawer
+                variant="temporary"
+                anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+                open={this.state.mobileOpen}
+                classes={{
+                  paper: classes.drawerPaper,
+                }}
+                onClose={this.handleDrawerToggle}
+                ModalProps={{
+                  keepMounted: true, // Better open performance on mobile.
+                }}
+              >
+                {drawer}
+              </Drawer>
+            </Hidden>
+            <Hidden smDown implementation="css">
+              <Drawer
+                variant="permanent"
+                open
+                classes={{
+                  paper: classes.drawerPaper,
+                }}
+              >
+                {drawer}
+              </Drawer>
+            </Hidden>
+            <main className={classes.content}>
+              <Typography noWrap>{'You think water moves fast? You should see ice.'}</Typography>
+            </main>
+          </div>
+        </div>
+      </Grid>
+    );
+  }
+};
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(App);
