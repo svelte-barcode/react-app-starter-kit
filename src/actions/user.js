849536b25f0d63cf87f3f7911c95bb5301a264fc
Bunlong <bunlong.van@gmail.com>
2018-03-07 17:42:13 +0700

    create UserList Container

--- src/actions/user.js
+++ src/actions/user.js
@@ -3,11 +3,11 @@ import axios from "axios";
 import { FETCH_USERS } from '../constants/user';
 
 export function fetchUsers () {
-  const request = axios.get(`./db.json`);
+  const request = axios.get(`./db.json`, { headers: { 'Content-Type': 'application/json' } });
 
   return (dispatch) => {
     request.then((response) => {
-      dispatch({ type: FETCH_USERS, payload: response.data })
+      dispatch({ type: FETCH_USERS, payload: eval(response.data) })
     }).catch((error) => {
       console.log('Error comming ', error);
     });
--- src/views/users/index.js
+++ src/views/users/index.js
@@ -1,36 +1,15 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 
-import { fetchUsers } from '../../actions/user';
+import UserListContainer from '../../containers/UserListContainer';
 
 class Index extends Component {
-  componentDidMount() {
-    this.props.fetchUsers();
-  }
-
   render() {
-    const { users } = this.props;
-    
-    if(users == undefined) {
-      console.log("loading");
-    } else {
-      console.log(users);
-    }
-
     return (
       <div>
-        User list
+        <UserListContainer />
       </div>
     );
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    users: state.user.all,
-  };
-}
-
-export default connect(mapStateToProps, { fetchUsers })(Index);
-
-// export default Index;
+export default Index;
